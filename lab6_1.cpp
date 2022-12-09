3f02d573968b9b784c7d9881833edffd7d006608
vasiree <vasiree_nan@cmu.ac.th>
2022-12-09 15:32:33 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
+    int num, i = 0, j = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num;
+    while (num!=0)
+    {
+        if(num%2==0){
+            i = i+1;
+                    }
+        else{
+            j = j+1;
+        }
+    cout << "Enter an integer: ";
+    cin >> num;
+    }
+
+    cout << "#Even numbers = " << i <<"\n";
+    cout << "#Odd numbers = " << j <<"\n";
     return 0;
 }
